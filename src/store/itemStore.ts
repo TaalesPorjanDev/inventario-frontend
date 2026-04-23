306841f6582dd72ddf4e40dd3f8186e0462b9825
Tales Porjan <taales.porjandev@gmail.com>
2026-04-23 08:52:25 -0300

    feat(store): implementation of the store with loading, adding, and removing items.

--- src/store/itemStore.ts
+++ src/store/itemStore.ts
@@ -3,18 +3,46 @@ import type { Item } from '../types/item'
 
 
 interface ItemStore {
-    items: Item[]
+    itens: Item[]
     adicionarItem: (item: Omit<Item, 'id'>) => void;
     removerItem: (id: string) => void;
     carregarItens: () => void;
 }
 
-export const useItemStore = create<ItemStore>((set) => ({
+export const useItemStore = create<ItemStore>((set,get) => ({
   itens: [],
-  adicionarItem(item) {},
-  removerItem(id) {},
+  
+  adicionarItem(item) {
+    const novoId = Date.now().toString()
+    const itemCompleto = {...item, id: novoId }
+    const listaAtual = get().itens
+    const novaLista = [...listaAtual, itemCompleto]
+    set({itens: novaLista})
+    localStorage.setItem("itens", JSON.stringify(novaLista))
+  },
+  
+  removerItem(id) {
+    
+    // pegar a lista atual de itens usando get().itens
+    const listaAtual = get().itens
+
+    // filtrar a lista mantendo itens cujo id seja diferente do id recebido
+    const novaLista = listaAtual.filter(item => item.id !== id)
+
+    // atualizar a store com a nova lista filtrada
+    set({itens: novaLista})
+
+    // salvar a nova lista no localStore
+    localStorage.setItem('itens', JSON.stringify(novaLista))
+  },
+  
   carregarItens() {
-   
+    const itens = localStorage.getItem('itens')
+    if(itens !== null) {
+      set({itens: JSON.parse(itens)})
+    } else {
+      set({itens: []})
+    }
   }
 }))
 
