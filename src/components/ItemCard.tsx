fd1f8470df044dff2f8124c080155a82d3e7bdff
Tales Porjan <taales.porjandev@gmail.com>
2026-04-28 18:37:38 -0300

    feat: style(HomePage, ItemCard): adjusts ItemCard colors and HomePage background according to design

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,5 +1,5 @@
 import type { Item } from '../types/item'
-import { Trash2 } from 'lucide-react';
+import { Trash2, Pencil } from 'lucide-react';
 
 interface ItemCardProps {
     item: Item;
@@ -7,12 +7,24 @@ interface ItemCardProps {
 }
 export function ItemCard({item, onDelete}: ItemCardProps) {
     return (
-        <div className='bg-gray-50 rounded-lg shadow-md p-4'>
+        <div className='bg-white rounded-lg shadow-md p-4'>
             <div className='flex justify-between items-start'>
-                <h3 className='text-lg font-semibold text-blue-600'>{item.nome}</h3>
-                <Trash2 className='text-red-500 h-5 w-5 cursor-pointer' onClick={onDelete}></Trash2>
+                <h3 className='text-2xl font-semibold text-gray-900'>{item.nome}</h3>
+                <div className='flex gap-2'>
+                    <Pencil className='text-gray-400 h-5 w-5 cursor-pointer'/>
+                    <Trash2 className='text-red-500 h-5 w-5 cursor-pointer' onClick={onDelete}/>
+                </div>
             </div>
-            <p className='text-sm text-gray-500 mt-1'>{item.categoria} • {item.local}</p>
-        </div>
+            <div className='flex gap-2 mt-2'>
+                <span className='px-2 py-1 border border-gray-200 rounded-full text-xs text-[#505f76]'>
+                    {item.categoria}
+                </span>
+                <span className='px-2 py-1 border border-gray-200 rounded-full text-xs text-[#505f76]'>
+                    {item.local}
+                </span>
+
+            </div>
+            
+        </div> 
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,31 +2,31 @@ import { useEffect } from 'react';
 import { useItemStore } from '../store/itemStore';
 import { ItemCard } from '../components/ItemCard';
 
-
-
 export function HomePage() {
   const { itens, removerItem, carregarItens } = useItemStore();
 
   useEffect(() => {
     carregarItens();
   }, []);
-  
+
   return (
-    <div className="container mx-auto p-4 pt-20"> {/* pt-20 para não ficar atrás do menu */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {itens.map((item) => (
-          <ItemCard
-            key={item.id}
-            item={item}
-            onDelete={() => removerItem(item.id)}
-          />
-        ))}
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto p-4 pt-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {itens.map((item) => (
+            <ItemCard
+              key={item.id}
+              item={item}
+              onDelete={() => removerItem(item.id)}
+            />
+          ))}
+        </div>
+        {itens.length === 0 && (
+          <p className="text-center text-gray-500 mt-10">
+            Nenhum item cadastrado
+          </p>
+        )}
       </div>
-      {itens.length === 0 && (
-        <p className="text-center text-gray-500 mt-10">
-          Nenhum item cadastrado
-        </p>
-      )}
     </div>
   );
 }
