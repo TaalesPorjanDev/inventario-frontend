aa300ff322545c546eedb7156639db00ebb49287
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-05-08 11:50:43 -0300

    refactor(App): Organize imports and improve component structure for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import { Routes, Route } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
-import { ItemForm } from './components/ItemForm'
-import { EditarItem } from './components/EditarItem'
+import { Routes, Route } from "react-router-dom";
 
+import { HomePage } from "./pages/HomePage";
+import { Login } from "./pages/Login";
 
-import { Login } from './pages/Login'
-import { ProtectedRoute } from './components/ProtectedRoute'
-import { Layout } from './components/Layout'
+import { ItemForm } from "./components/ItemForm";
+import { EditarItem } from "./components/EditarItem";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { Layout } from "./components/Layout";
+import { Toast } from "./components/Toast";
+
+import { useToastStore } from "./store/toastStore";
 
 export function App() {
-  
+  const { visible, message, type, hideToast } = useToastStore();
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-  
-      <Route element={<Layout />}>
-        <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-        <Route path="/adicionar" element={<ProtectedRoute><ItemForm /></ProtectedRoute>} />
-        <Route path="/editar/:id" element={<ProtectedRoute><EditarItem /></ProtectedRoute>} />
-      </Route>
-    </Routes>
-    
-  )
-}
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/adicionar"
+            element={
+              <ProtectedRoute>
+                <ItemForm />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/editar/:id"
+            element={
+              <ProtectedRoute>
+                <EditarItem />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+
+      {visible && <Toast message={message} type={type} onClose={hideToast} />}
+    </>
+  );
+}
