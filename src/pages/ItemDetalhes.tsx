4ea0a80cf8e04b4bfcc12aee8ff5e54b37c13046
Tales Porjan <taales.porjandev@gmail.com>
2026-05-09 09:46:53 -0300

    feat(ItemDetahes): correction of local/category display.

--- src/pages/ItemDetalhes.tsx
+++ src/pages/ItemDetalhes.tsx
@@ -1,35 +1,42 @@
-import { useParams, useNavigate } from "react-router-dom"
-import { useEffect } from "react"
-import { useItemStore } from "../store/itemStore"
-/* import { Item } from "../types/item" */
+import { useParams, useNavigate } from 'react-router-dom';
+import { useItemStore } from '../store/itemStore';
 
 export function ItemDetalhes() {
-    const navigate = useNavigate()
-    const { itens } = useItemStore()
-    const { id } = useParams<{id: string}>()
-    
-    const item = itens.find(item => item.id === id)
-    if(!item) {
-        navigate('/')
-        return null
-    }
+  const navigate = useNavigate();
+  const { itens } = useItemStore();
+  const { id } = useParams<{ id: string }>();
 
-    return (
-        <main className="min-h-screen bg-gray-50 py-8">
-            <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-md p-6">
-                <h1 className="text-2xl font-bold text-gray-900 mb-4">{item.nome}</h1>
-                <p className="text-gray-700 mb-2"><strong className="font-semibold">Local:</strong>{item.categoria}</p>
-                <p className="text-gray-700 mb-2"><strong className="font-semibold">Categoria:</strong>{item.categoria}</p>
+  const item = itens.find((item) => item.id === id);
+  if (!item) {
+    navigate('/');
+    return null;
+  }
 
-                {item.observacao && (
-                    <div>
-                        <strong className="font-semibold">Observação:</strong>
-                        <p className="text-gray-700 mb-2">{item.observacao}</p>
-                    </div>
-                )}
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-md p-6">
+        <h1 className="text-2xl font-bold text-gray-900 mb-4">{item.nome}</h1>
+        <p className="text-gray-700 mb-2">
+          <strong className="font-semibold">Local:</strong> {item.local}
+        </p>
+        <p className="text-gray-700 mb-2">
+          <strong className="font-semibold">Categoria:</strong> {item.categoria}
+        </p>
 
-                <button className="mt-6 bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded-md" onClick={() => navigate('/')}>Voltar</button>
-            </div>
-        </main>
-    )
+        {item.observacao && (
+          <div>
+            <strong className="font-semibold">Observação:</strong>
+            <p className="text-gray-700 mb-2">{item.observacao}</p>
+          </div>
+        )}
+
+        <button
+          className="mt-6 bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded-md"
+          onClick={() => navigate('/')}
+        >
+          Voltar
+        </button>
+      </div>
+    </main>
+  );
 }
